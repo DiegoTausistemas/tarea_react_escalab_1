69dab68915dde80170766e92d9e841dbaf227a94
Diego Jimenez <diego@tausistemas.cl>
2021-08-13 16:13:11 -0400

    Primeros avances, routes, props, vista, render, class, funcional component, data

--- src/pages/welcome/Welcome.component.jsx
+++ src/pages/welcome/Welcome.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Paper, Grid } from "@material-ui/core";
+import Intro from "../../components/intro/Intro.component";
 
 const Welcome = () => (
   <div className="row align-items-center vh-100">
     <Grid container direction="row" justifyContent="center">
       <Grid item xs={6}>
-        <Paper elevation={0} className="border rounded text-center">
-          Welcome
+        <Paper elevation={1} className="border rounded text-center">
+          <Intro />
         </Paper>
       </Grid>
     </Grid>
